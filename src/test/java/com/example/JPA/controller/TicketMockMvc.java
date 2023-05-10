f1cf550dbb1334113967245f39921600c08a47d4
Varadi-Marcell <marcibazsi11@gmail.com>
2023-05-10 17:39:34 +0200

    Integration tests added

--- src/test/java/com/example/JPA/controller/TicketMockMvc.java
+++ src/test/java/com/example/JPA/controller/TicketMockMvc.java
@@ -1,9 +1,14 @@
 package com.example.JPA.controller;
 
+import com.example.JPA.auth.AuthenticationRequest;
+import com.example.JPA.auth.AuthenticationResponse;
+import com.example.JPA.dto.user.UserPersonalDetailsDto;
 import com.example.JPA.model.Ticket;
 import com.example.JPA.repository.TicketRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +19,7 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class TicketMockMvc {
     @Autowired
     private MockMvc mockMvc;
@@ -30,9 +37,29 @@ public class TicketMockMvc {
     @Autowired
     TicketRepository ticketRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String jwtToken;
+
+
     @BeforeEach
-    void init() {
+    public void setUp() throws Exception {
+        AuthenticationRequest authRequest = new AuthenticationRequest("asd@mail.com", "asdasd");
+        String authRequestBody = objectMapper.writeValueAsString(authRequest);
+
+        String response = mockMvc.perform(post("/api/v1/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authRequestBody))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        AuthenticationResponse authResponse = objectMapper.readValue(response, AuthenticationResponse.class);
+        jwtToken = authResponse.getToken();
     }
+
 
     @Test
     public void shouldThrowBadRequest() throws Exception {
@@ -43,8 +70,9 @@ public class TicketMockMvc {
 
     @Test
     public void shouldReturnTicketById() throws Exception {
-        Ticket ticket = ticketRepository.save(new Ticket("Koncert", "Budapest", LocalDate.of(2023, Month.JANUARY, 1), LocalDate.of(2023, Month.MARCH, 1), 430, "Rock"));
-        this.mockMvc.perform(get("/api/v1/ticket/101"))
+        Ticket ticket = ticketRepository.save(new Ticket("Koncert32", "Budapest", LocalDate.of(2023, Month.JANUARY, 1), LocalDate.of(2023, Month.MARCH, 1), 430, "Rock"));
+
+        this.mockMvc.perform(get("/api/v1/ticket/"+ticket.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
@@ -57,12 +85,11 @@ public class TicketMockMvc {
     public void shouldCreateTicket() throws Exception {
         Ticket ticket = new Ticket("Koncert", "Budapest", LocalDate.of(2023, Month.JANUARY, 1), LocalDate.of(2023, Month.MARCH, 1), 430, "Rock");
 
-
         this.mockMvc.perform(post("/api/v1/ticket")
+                        .header("Authorization", "Bearer " + jwtToken)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(String.valueOf(ticket)))
+                        .content(objectMapper.writeValueAsString(ticket)))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
-
     }
 }
